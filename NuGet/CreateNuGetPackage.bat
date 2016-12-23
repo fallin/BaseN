96d9e0a7e76075254d34984134ca6d20b2c487bc
Brant Fallin <brant.fallin@gmail.com>
2016-12-23 13:58:42 -0700

    Rearrange files
    - Move code under src/ folder
    - Remove NuGet folder, nuspec under root
    - Update build and deploy (to NuGet) script

--- NuGet/CreateNuGetPackage.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-:: Default to current path when running as admin user
-cd /d %~dp0
-
-:: Assuming the nuget.exe is in your path
-nuget pack BaseN.nuspec
---
+++ deploy.cmd
@@ -0,0 +1,29 @@
+@echo off
+@cd /d "%~dp0"
+
+echo info: Setup MSBuild environment
+call "%VS140COMNTOOLS%VsMSBuildCmd.bat"
+
+echo info: Build BaseN
+msbuild.exe ".\src\BaseN.sln" /p:Configuration=Release /t:Rebuild
+if ERRORLEVEL 1 (
+    echo error: Failed to build BaseN
+    goto EXIT
+)
+
+echo info: Pack NuGet nuspec
+for /f %%a IN ('dir /b *.nuspec') do (
+	nuget pack "%%a"
+)
+
+echo info: Push NuGet nupkg
+set /p APIKEY="Enter NuGet ApiKey:"
+for /f %%b IN ('dir /b *.nupkg') do (
+	:: nuget push "%%b" -ApiKey %APIKEY% -Source https://www.nuget.org/api/v2/package
+)
+
+mkdir .\build
+move /y *.nupkg .\build
+
+:EXIT
+pause
