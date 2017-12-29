b70fa083680367f5c21d3cb4b108f5996a6a90b5
Brant Fallin <brant.fallin@gmail.com>
2017-12-29 15:10:07 -0700

    Update to new VS2017 csproj format

---
+++ package.cmd
@@ -0,0 +1,43 @@
+@echo off
+@cd /d "%~dp0"
+
+echo info: Package and release Itc4net NuGet package
+
+echo info: Download vswhere (to locate msbuild)
+mkdir .\build
+set VSWHERE_URL=https://github.com/Microsoft/vswhere/releases/download/1.0.62/vswhere.exe
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "Invoke-WebRequest -Uri %VSWHERE_URL% -OutFile '.\build\vswhere.exe'"
+if ERRORLEVEL 1 (
+    echo error: Failed to download vswhere
+    goto EXIT
+)
+
+:: See https://github.com/Microsoft/vswhere
+for /f "usebackq tokens=1* delims=: " %%i in (`.\build\vswhere -latest -requires Microsoft.Component.MSBuild`) do (
+  if /i "%%i"=="installationPath" set VSCOMNTOOLS=%%j
+)
+
+echo info: Setup MSBuild environment
+:: This batch script modifies the current directory
+set PRESERVE_CURRENT_DIRECTORY=%~dp0
+call "%VSCOMNTOOLS%\Common7\tools\VsMSBuildCmd.bat"
+cd /d %PRESERVE_CURRENT_DIRECTORY%
+set PRESERVE_CURRENT_DIRECTORY=
+
+echo info: Build BaseN
+msbuild.exe ".\src\BaseN.sln" /p:Configuration=Release /t:Rebuild
+if ERRORLEVEL 1 (
+    echo error: Failed to build BaseN
+    goto EXIT
+)
+
+xcopy /y /r ".\src\BaseN\bin\Release\*.nupkg" ".\build"
+
+:: echo info: Push NuGet nupkg
+:: set /p APIKEY="Enter NuGet ApiKey:"
+:: for /f %%b IN ('dir /b *.nupkg') do (
+::     nuget push "%%b" -ApiKey %APIKEY% -Source https://www.nuget.org/api/v2/package
+:: )
+
+:EXIT
+pause
--- src/BaseN/Properties/AssemblyInfo.cs
+++ src/BaseN/Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("BaseN")]
-[assembly: AssemblyDescription("Various binary-to-text data encodings.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BaseN")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -21,18 +9,5 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("45610185-b6d7-4341-a950-7574e4caab75")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: InternalsVisibleTo("BaseN.Tests")]
